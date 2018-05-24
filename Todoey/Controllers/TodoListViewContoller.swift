ed6259b225764ebd78116ba900d613525142c616
mamorto2 <mm.m7769@gmail.com>
2018-05-23 20:51:38 -0400

    save and recall data from Plist

--- Todoey/Controllers/TodoListViewContoller.swift
+++ Todoey/Controllers/TodoListViewContoller.swift
@@ -23,25 +23,8 @@ class TodoListViewController: UITableViewController {
         
 
 //        print(dataFilePath)
-        
-        
-        
-        
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-    
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
-        
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//            itemArray = items
-//        }
+     
+        loadItems()
         
         
     }
@@ -121,7 +104,16 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+           let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    }
     
     
 }
